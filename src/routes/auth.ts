da2274ca16a37d8ac5ecbe6892c0145826acdc8f
Nicolas Pereira <nicolas.pp333@gmail.com>
2025-02-02 13:28:20 -0300

    feat: add validation middleware and schemas for login and registration endpoints

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { login, register } from '../controllers/auth';
+import { validateBody } from '../middlewares/validate';
+import {
+  loginBodySchema,
+  registerBodySchema,
+} from '../middlewares/validation_schemas/auth';
 
 const router = Router();
 
-router.post('/login', login);
-router.post('/register', register);
+router.post('/login', validateBody(registerBodySchema), login);
+router.post('/register', validateBody(loginBodySchema), register);
 
 export default router;
