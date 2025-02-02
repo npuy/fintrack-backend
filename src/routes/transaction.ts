44de209008ef1922c40d6e4fa5d7840003eb4a67
Nicolas Pereira <nicolas.pp333@gmail.com>
2025-02-02 13:37:32 -0300

    feat: add validation schemas and middleware for transaction and user

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -10,16 +10,31 @@ import {
   updateTransaction,
 } from '../controllers/transaction';
 import { verifyToken } from '../middlewares/auth';
+import { validateBody } from '../middlewares/validate';
+import {
+  createTransactionBodySchema,
+  updateTransactionBodySchema,
+} from '../middlewares/validation_schemas/transaction';
 
 const router = Router();
 
-router.post('/', verifyToken, createTransaction);
+router.post(
+  '/',
+  verifyToken,
+  validateBody(createTransactionBodySchema),
+  createTransaction,
+);
 router.get('/', verifyToken, getTransactions);
 router.get('/full', verifyToken, getTransactionsFull);
 router.get('/acc/:accountId', verifyToken, getTransactionsByAccount);
 router.get('/cat/:categoryId', verifyToken, getTransactionsByCategory);
 router.get('/:transactionId', verifyToken, getTransactionById);
-router.put('/:transactionId', verifyToken, updateTransaction);
+router.put(
+  '/:transactionId',
+  verifyToken,
+  validateBody(updateTransactionBodySchema),
+  updateTransaction,
+);
 router.delete('/:transactionId', verifyToken, deleteTransaction);
 
 export default router;
