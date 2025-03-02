5f8134864191c301a61941da71b01c950fed025a
Nicolas Pereira <nicolas.pp333@gmail.com>
2025-03-02 20:30:28 -0300

    fix: change budget group service to calculate amounts in budget group currency

--- src/services/budget.ts
+++ src/services/budget.ts
@@ -7,7 +7,7 @@ import {
   getBudgetGroupsDB,
   updateBudgetGroupDB,
 } from '../models/budget';
-import { getCurrencyByIdDB } from '../models/currency';
+import { getCurrenciesDB, getCurrencyByIdDB } from '../models/currency';
 import {
   BudgetGroup,
   BudgetGroupWithCategories,
@@ -17,6 +17,8 @@ import {
 } from '../types/budget';
 import { getCategoriesByUserWithBalance } from './category';
 import { CategoryWithBalance } from '../types/category';
+import { Currency } from '../types/account';
+import { getUserByIdDB } from '../models/user';
 
 export async function createBudgetGroupService(
   createBudgetGroupInput: CreateBudgetGroupInput,
@@ -36,39 +38,62 @@ export async function createBudgetGroupService(
   return budgetGroup;
 }
 
-function getBudgetGroupWithAmount(
-  budgetGroup: BudgetGroupWithCategories,
-  categories: CategoryWithBalance[],
-): BudgetGroupWithCategoriesAndAmount {
-  const categoriesIdsFormGroup = new Set(
-    budgetGroup.categories.map((category) => category.id),
+async function getBudgetGroupWithAmount(
+  budgetGroups: BudgetGroupWithCategories[],
+  userId: string,
+): Promise<BudgetGroupWithCategoriesAndAmount[]> {
+  // Get all categories with balance
+  const categories = await getCategoriesByUserWithBalance(
+    userId,
+    undefined,
+    undefined,
   );
+  const currencies = await getCurrenciesDB();
 
-  const amount = categories
-    .filter((category) => categoriesIdsFormGroup.has(category.id))
-    .reduce((add, category) => add + category.balance, 0);
+  // Get user currency to change the currency of the amount
+  const user = await getUserByIdDB(userId);
+  if (!user) {
+    throw new ValueNotFoundError('User not found');
+  }
+  const currency = await getCurrencyByIdDB(user.currencyId);
+  if (!currency) {
+    throw new ValueNotFoundError('Currency not found');
+  }
 
-  return {
-    ...budgetGroup,
-    amount,
-  };
+  const budgetGroupsWithAmount = budgetGroups.map((budgetGroup) => {
+    const categoriesIdsFormGroup = new Set(
+      budgetGroup.categories.map((category) => category.id),
+    );
+
+    // Calculate the amount of the budget group (in user currency)
+    let amount = categories
+      .filter((category) => categoriesIdsFormGroup.has(category.id))
+      .reduce((add, category) => add + category.balance, 0);
+
+    // Change the currency of the amount to the budget group currency
+    const budgetGroupCurrency = currencies.find(
+      (currency) => currency.id == budgetGroup.currencyId,
+    );
+    if (!budgetGroupCurrency) {
+      throw new ValueNotFoundError('Currency not found');
+    }
+    amount = (amount * budgetGroupCurrency.multiplier) / currency.multiplier;
+
+    return {
+      ...budgetGroup,
+      amount,
+    };
+  });
+
+  return budgetGroupsWithAmount;
 }
 
 export async function getBudgetGroupsService(
   userId: string,
 ): Promise<BudgetGroupWithCategoriesAndAmount[]> {
   const budgetGroups = await getBudgetGroupsDB(userId);
-  const categories = await getCategoriesByUserWithBalance(
-    userId,
-    undefined,
-    undefined,
-  );
 
-  const budgetGroupsWithAmount = budgetGroups.map((budgetGroup) => {
-    return getBudgetGroupWithAmount(budgetGroup, categories);
-  });
-
-  return budgetGroupsWithAmount;
+  return await getBudgetGroupWithAmount(budgetGroups, userId);
 }
 
 export async function getBudgetGroupByIdService(
@@ -81,12 +106,7 @@ export async function getBudgetGroupByIdService(
     throw new ValueNotFoundError('Budget group not found');
   }
 
-  const categories = await getCategoriesByUserWithBalance(
-    userId,
-    undefined,
-    undefined,
-  );
-  return getBudgetGroupWithAmount(budgetGroup, categories);
+  return (await getBudgetGroupWithAmount([budgetGroup], userId))[0];
 }
 
 export async function updateBudgetGroupService(
